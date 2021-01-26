7f465b4b03af583acf808fa290e775c2c0e11a62
EmreErdem <emre.erdm00@gmail.com>
2021-01-26 14:04:37 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> Musteriler = new List<Musteri>();
+        public void MusteriEkle(Musteri musteri)
+        {
+            int Id = musteri.Id;
+            string Ad = musteri.Ad;
+            string Soyad = musteri.Soyad;
+            int age = musteri.Age;
+            Console.WriteLine(musteri.Ad +" " + "başarıyla eklenmiştir");
+            Console.WriteLine("--------------------------------------------");
+            Musteriler.Add(musteri);
+            
+            
+        }
+
+        public void MusteriListele()
+        {
+            foreach (var item in Musteriler)
+            {
+                Console.WriteLine( item.Ad +" "+ item.Soyad + " "+item.Age);
+                Console.WriteLine("-----------------------------------------------");
+
+            }
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+           
+                Console.WriteLine(musteri.Ad + " isme sahip müşteri silindi.");
+            
+            
+               
+                
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Emre";
+            musteri1.Soyad = "Erdem";
+            musteri1.Age = 21;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Nida";
+            musteri2.Soyad = "Erdem";
+            musteri2.Age = 21;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Emel";
+            musteri3.Soyad = "Erdem";
+            musteri3.Age = 27;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+            musteriManager.MusteriListele();
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriListele();
+
+
+            Console.ReadKey();
+        }
+    }
+}
